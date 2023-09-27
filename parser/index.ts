0882a642419a51b8e7dfb24c5ad32b29aaf71033
Robert Young <robertyoung10101@gmail.com>
2023-09-27 21:46:08 +1300

    Split books and chapters

--- parser/index.ts
+++ parser/index.ts
@@ -1,4 +1,36 @@
+async function parseGotm() {
+    const src = Bun.file("./text/gotm.txt");
+    const rawText = await src.text();
 
-const src = Bun.file("./text/gotm.txt");
-src.text()
-    .then(text => console.log(text));
+    const books = splitByHeader(rawText, ["## BOOK", "### Prologue", "### EPILOGUE"])
+        .map(book => {
+            const heading = book.match(/## ([^\r?\n]*)/)![1];
+            const chapters = book.startsWith("## BOOK") ?
+                splitByHeader(book, ["### CHAPTER"]) : [book];
+
+            return {
+                heading,
+                chapters,
+                chapterCount: chapters.length,
+            }
+        });
+    
+    console.dir(books);
+}
+
+function splitByHeader(text: string, headers: string[]): string[] {
+    const chunks: string[] = [];
+
+    while (true) {
+        const index = Math.max(...headers.map(header => text.lastIndexOf(header)));
+
+        if (index === -1) {
+            return chunks.reverse();
+        }
+
+        chunks.push(text.slice(index));
+        text = text.slice(0, index);
+    }
+}
+
+parseGotm();
