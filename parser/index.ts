8e232b6e62c59fe96ca1cf3e8415f16ec20efbd2
Robert Young <robertyoung10101@gmail.com>
2023-10-02 22:05:08 +1300

    Split chapter text into scenes

--- parser/index.ts
+++ parser/index.ts
@@ -2,20 +2,36 @@ async function parseGotm() {
     const src = Bun.file("./text/gotm.txt");
     const rawText = await src.text();
 
-    const books = splitByHeader(rawText, ["## BOOK", "### Prologue", "### EPILOGUE"])
+    const sanitisedText = removeSpecialCharacters(rawText);
+    const books = splitByHeader(sanitisedText, ["## BOOK", "### Prologue", "### EPILOGUE"])
         .map(book => {
-            const heading = book.match(/## ([^\r?\n]*)/)![1];
+            const heading = book.match(/## ([^\n]*)/)![1];
             const chapters = book.startsWith("## BOOK") ?
                 splitByHeader(book, ["### CHAPTER"]) : [book];
 
             return {
                 heading,
-                chapters,
+                chapters: chapters.map(c => parseChapter(c)),
                 chapterCount: chapters.length,
             }
         });
     
     console.dir(books);
+}
+
+function removeSpecialCharacters(text: string): string {
+    return text.replaceAll(/(\r)|(\*)|(\\)/g, "");
+}
+
+function parseChapter(chapterText: string) {
+    const heading = chapterText.match(/### ([^\n]*)/)![1];
+    const scenes = chapterText.split("\n\n\n");
+
+    return {
+        heading,
+        excerpt: scenes[1],
+        scenes: scenes.slice(2),
+    }
 }
 
 function splitByHeader(text: string, headers: string[]): string[] {
